64b9781f32527243f9b741a7529995a12a150943
Brian Mueller <brianscottmueller@gmail.com>
2017-10-11 13:19:27 +0000

    test highlight column

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,13 @@
-$(function(){
+$(function() {
+    
+    // clicking on a cell highlights the entire column
+    $('td').on('click', function() {
+        var $currentTable = $(this).closest('table');
+        var index = $(this).index();
+        $currentTable.find('td').removeClass('selected');
+        $currentTable.find('tr').each(function() {
+            $(this).find('td').eq(index).addClass('selected');
+        });
+    });
     
 });
