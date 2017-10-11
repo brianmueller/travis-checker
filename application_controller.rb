d6f68254d87c5a9c5650cf382ce7e51887e4b300
Brian Mueller <brianscottmueller@gmail.com>
2017-10-11 12:16:46 +0000

    dynamic routes

--- application_controller.rb
+++ application_controller.rb
@@ -37,4 +37,34 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
   
+  get "/:repo" do
+    user_repo_url = "https://github.com/bmuellerhstat/#{params[:repo]}"
+    user_name = "bmuellerhstat"
+    user_repo = get_repo_name(user_repo_url)
+    user_name_repo = "#{user_name}/#{user_repo}"
+    
+    get_prs(user_name_repo)
+    Pr.all.each do |pr|
+        get_info(pr,user_name_repo,pr.sha)
+    end
+    
+    Student.all.sort_by{|student| student.first}
+    
+    Student.all.each do |student|
+      matching_pr = Pr.all.find{|pr| pr.username == student.username}
+      unless matching_pr.nil?
+        student.sha = matching_pr.sha
+        student.url = matching_pr.url
+        student.status = matching_pr.status
+        student.timestamp = matching_pr.timestamp
+      end
+    end
+    
+    Student.all.sort_by{|student| student.last}
+    
+    
+    
+    erb :index
+  end
+  
 end
